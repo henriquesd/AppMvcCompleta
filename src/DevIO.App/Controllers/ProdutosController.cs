fd53ceb484850ecbd432f0cbcef6259d539f7e54
HenriqueSD <hsd.foz@gmail.com>
2019-08-16 23:46:46 -0300

    Finalizando o ajuste das Views e correção no ProdutosController

--- src/DevIO.App/Controllers/ProdutosController.cs
+++ src/DevIO.App/Controllers/ProdutosController.cs
@@ -117,7 +117,7 @@ namespace DevIO.App.Controllers
 
         public async Task<IActionResult> Delete(Guid id)
         {
-            var produto = ObterProduto(id);
+            var produto = await ObterProduto(id);
 
             if (produto == null)
             {
@@ -131,7 +131,7 @@ namespace DevIO.App.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            var produto = ObterProduto(id);
+            var produto = await ObterProduto(id);
 
             if (produto == null)
             {
